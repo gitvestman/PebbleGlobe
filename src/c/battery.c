d18878f6a3fd433f50c1e4d870395f28a6418944
Peter Vestman <peter@vestman.net>
2016-09-16 20:00:05 +0200

    Show battery icon in inverted bw display

--- src/c/battery.c
+++ src/c/battery.c
@@ -1,5 +1,6 @@
 #include <pebble.h>
 #include "battery.h"
+#include "message.h"
 
 static Layer *s_battery_layer;
 static BatteryChargeState charge_state;
@@ -19,15 +20,19 @@ static void handle_battery(BatteryChargeState state) {
 
 void battery_layer_update_callback(Layer *layer, GContext *ctx) {
   // Draw the battery:
-  graphics_context_set_stroke_color(ctx,
-    COLOR_FALLBACK((charge_state.charge_percent > 20 ? GColorIslamicGreen : GColorRed) , GColorWhite));
+  #ifdef PBL_BW  
+  GColor iconcolor = app_config.inverted ? GColorBlack : GColorWhite;
+  #else
+  GColor iconcolor = COLOR_FALLBACK((charge_state.charge_percent > 20 ? GColorIslamicGreen : GColorRed) , GColorWhite);
+  #endif
+
+  graphics_context_set_stroke_color(ctx, iconcolor);
   graphics_draw_rect(ctx, GRect(6, 4, 14, 8));
   graphics_draw_line(ctx, GPoint(20, 6), GPoint(20, 9));
 
   charge_state = battery_state_service_peek();
   int width = (charge_state.charge_percent * 10 / 100);
-  graphics_context_set_fill_color(ctx,
-    COLOR_FALLBACK((charge_state.charge_percent > 20 ? GColorIslamicGreen : GColorRed), GColorWhite));
+  graphics_context_set_fill_color(ctx, iconcolor);
   graphics_fill_rect(ctx, GRect(8, 6, width, 4), 0, GCornerNone);
 }
 
